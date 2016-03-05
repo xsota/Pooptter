2af8d3e5a11ff0e29f35c2d2ac87cb61ffa1d940
sota <git@xsota.com>
2016-03-05 23:48:20 +0900

    とりあえずTwitterにログインしてuserNameをログに吐き出すところまで成功

--- app/src/main/java/com/xsota/pooptter/pooptter/MainActivity.java
+++ app/src/main/java/com/xsota/pooptter/pooptter/MainActivity.java
@@ -1,18 +1,30 @@
 package com.xsota.pooptter.pooptter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-    private static final String TWITTER_KEY = "HXKR3lMzq8hJIV3Xp3UycH1MC";
-    private static final String TWITTER_SECRET = "xSnq78vYSOdBgem0gaixsaNOchv6YxCYADFw9OymRiZY5KUAxL";
+  @Bind(R.id.login_button)
+  TwitterLoginButton twitterLoginButton;
+
+  // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+  private static final String TWITTER_KEY = "HXKR3lMzq8hJIV3Xp3UycH1MC";
+  private static final String TWITTER_SECRET = "xSnq78vYSOdBgem0gaixsaNOchv6YxCYADFw9OymRiZY5KUAxL";
 
 
   @Override
@@ -21,27 +33,26 @@ public class MainActivity extends AppCompatActivity {
     TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
     Fabric.with(this, new Twitter(authConfig));
     setContentView(R.layout.activity_main);
+
+    ButterKnife.bind(this);
+
+    twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+      @Override
+      public void success(Result<TwitterSession> result) {
+        Log.d("MainActivity", "せいこう");
+        Log.d("userName", result.data.getUserName());
+      }
+
+      @Override
+      public void failure(TwitterException e) {
+        Log.d("しっぱい", e.getMessage());
+      }
+    });
   }
 
   @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    twitterLoginButton.onActivityResult(requestCode, resultCode, data);
   }
 }
