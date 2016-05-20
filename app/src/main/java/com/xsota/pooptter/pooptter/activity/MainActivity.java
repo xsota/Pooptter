a9443cacc3bfe7c42d8e0128439bc579e9019750
sota <git@xsota.com>
2016-05-21 00:47:09 +0900

    ただhomeTimeLineを取得して表示するだけ

--- app/src/main/java/com/xsota/pooptter/pooptter/activity/MainActivity.java
+++ app/src/main/java/com/xsota/pooptter/pooptter/activity/MainActivity.java
@@ -2,13 +2,23 @@ package com.xsota.pooptter.pooptter.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 
 import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterApiClient;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.tweetui.FixedTweetTimeline;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 import com.twitter.sdk.android.tweetui.UserTimeline;
 import com.xsota.pooptter.pooptter.R;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,22 +37,38 @@ public class MainActivity extends AppCompatActivity {
     Fabric.with(this, new Twitter(authConfig));
 
     setContentView(R.layout.activity_main);
-    ButterKnife.setDebug(true);
     ButterKnife.bind(this);
-    
 
-
-    //listView = (ListView) findViewById(R.id.listview);
 
     final UserTimeline userTimeline = new UserTimeline.Builder()
         .screenName("xsota")
         .build();
-    final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
-        .setTimeline(userTimeline)
-        .build();
 
 
-    listView.setAdapter(adapter);
+    TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
+    twitterApiClient.getStatusesService().homeTimeline(20, null, null, null,null,null, null, new Callback<List<Tweet>>() {
+
+      @Override
+      public void success(Result<List<Tweet>> listResult) {
+
+        final FixedTweetTimeline timeline = new FixedTweetTimeline.Builder()
+            .setTweets(listResult.data)
+            .build();
+
+       listView.setAdapter(new TweetTimelineListAdapter
+           .Builder(getApplicationContext())
+           .setTimeline(timeline)
+           .build()
+       );
+      }
+
+      @Override
+      public void failure(TwitterException e) {
+        Log.e(TAG, e.getMessage());
+      }
+    });
+
+
   }
 
 }
